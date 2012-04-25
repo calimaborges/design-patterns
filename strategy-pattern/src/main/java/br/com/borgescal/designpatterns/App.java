298820e829298cca3f75c14baec21f97dcb1b5f9
Carlos Augusto Lima Borges <calimaborges@gmail.com>
2012-04-25 02:49:26 -0300

    Commit inicial

---
+++ strategy-pattern/src/main/java/br/com/borgescal/designpatterns/App.java
@@ -0,0 +1,13 @@
+package br.com.borgescal.designpatterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
