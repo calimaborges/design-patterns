cfbbb2dc365486c532ac8eb6cce48a8831100a00
Carlos Augusto Lima Borges <calimaborges@gmail.com>
2012-05-01 15:18:59 -0300

    Adicionando template method pattern

---
+++ template-pattern/src/main/java/br/com/borgescal/designpatterns/template/CaffeineBeverage.java
@@ -0,0 +1,21 @@
+package br.com.borgescal.designpatterns.template;
+
+public abstract class CaffeineBeverage {
+	final void prepareRecipe() {
+		boilWater();
+		brew();
+		pourInCup();
+		addCondiments();
+	}
+	
+	abstract void brew();
+	abstract void addCondiments();
+	
+	void boilWater() {
+		System.out.println("Boiling water");
+	}
+	
+	void pourInCup() {
+		System.out.println("Pouring into cup");
+	}
+}
---
+++ template-pattern/src/main/java/br/com/borgescal/designpatterns/template/Coffee.java
@@ -0,0 +1,26 @@
+package br.com.borgescal.designpatterns.template;
+
+public class Coffee {
+	void prepareRecipe() {
+		boilWater();
+		brewCoffeeGrinds();
+		pourInCup();
+		addSugarAndMilk();
+	}
+	
+	public void boilWater() {
+		System.out.println("Boiling water");
+	}
+	
+	public void brewCoffeeGrinds() {
+		System.out.println("Dripping Coffee through filter");
+	}
+	
+	public void pourInCup() {
+		System.out.println("Pouring into cup");
+	}
+	
+	public void addSugarAndMilk() {
+		System.out.println("Adding Sugar and Milk");
+	}
+}
